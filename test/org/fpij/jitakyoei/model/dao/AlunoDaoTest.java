5113cbfe7ca24ec8a44e424c1d0030a1238e1823
Kenzo Sugai <kenzosugai@gmail.com>
2024-12-01 20:24:24 -0300

    Teste delete entidade issue id 6

--- test/org/fpij/jitakyoei/model/dao/AlunoDaoTest.java
+++ test/org/fpij/jitakyoei/model/dao/AlunoDaoTest.java
@@ -21,6 +21,7 @@ public class AlunoDaoTest {
 	private static DAO<Aluno> alunoDao;
 	private static Aluno aluno;
 	private static Entidade entidade;
+	private static DAO<Entidade> entidadeDao;
 	private static Endereco endereco;
 	private static Filiado f1;
 	private static Filiado filiadoProf;
@@ -76,71 +77,68 @@ public class AlunoDaoTest {
 	}
 	
 	
-	@Test
-	public void  testSalvarAlunoComAssociassoes() throws Exception{
-		clearDatabase();
+	// @Test
+	// public void  testSalvarAlunoComAssociassoes() throws Exception{
+	// 	clearDatabase();
 		
-		alunoDao.save(aluno);
-		assertEquals("036.464.453-27", alunoDao.get(aluno).getFiliado().getCpf());
-		assertEquals("Aécio", alunoDao.get(aluno).getFiliado().getNome());
-		assertEquals("Professor", alunoDao.get(aluno).getProfessor().getFiliado().getNome());
-		assertEquals("Dirceu", alunoDao.get(aluno).getProfessor().getFiliado().getEndereco().getBairro());
+	// 	alunoDao.save(aluno);
+	// 	assertEquals("036.464.453-27", alunoDao.get(aluno).getFiliado().getCpf());
+	// 	assertEquals("Aécio", alunoDao.get(aluno).getFiliado().getNome());
+	// 	assertEquals("Professor", alunoDao.get(aluno).getProfessor().getFiliado().getNome());
+	// 	assertEquals("Dirceu", alunoDao.get(aluno).getProfessor().getFiliado().getEndereco().getBairro());
+	// }
+	
+	// @Test
+	// public void updateAluno() throws Exception{
+	// 	clearDatabase();
+	// 	assertEquals(0, alunoDao.list().size());
+		
+	// 	alunoDao.save(aluno);
+	// 	assertEquals(1, alunoDao.list().size());
+	// 	assertEquals("Aécio", aluno.getFiliado().getNome());
+		
+	// 	Aluno a1 = alunoDao.get(aluno);
+	// 	a1.getFiliado().setNome("TesteUpdate");
+	// 	alunoDao.save(a1);
+		
+	// 	Aluno a2 = alunoDao.get(a1);
+	// 	assertEquals("TesteUpdate", a2.getFiliado().getNome());
+	// 	assertEquals(1, alunoDao.list().size());
+	// }
+
+	@Test
+	public void deleteEntidade() throws Exception{ // Teste ID 6
+
+		clearDatabase();
+		assertEquals(0, entidadeDao.list().size());
+		
+		entidadeDao.save(entidade);
+		assertEquals(1, entidadeDao.list().size());
+
+		Entidade e1 = entidadeDao.get(entidade);
+
+		entidadeDao.delete(e1);
+
+		assertEquals(0, entidadeDao.list().size());
 	}
 	
-	@Test
-	public void updateAluno() throws Exception{
-		clearDatabase();
-		assertEquals(0, alunoDao.list().size());
+	// @Test
+	// public void testSearchAluno() throws Exception{
+	// 	clearDatabase();
+	// 	alunoDao.save(aluno);
 		
-		alunoDao.save(aluno);
-		assertEquals(1, alunoDao.list().size());
-		assertEquals("Aécio", aluno.getFiliado().getNome());
+	// 	Filiado f = new Filiado();
+	// 	f.setNome("Aécio");
+	// 	Aluno a = new Aluno();
+	// 	a.setFiliado(f);
 		
-		Aluno a1 = alunoDao.get(aluno);
-		a1.getFiliado().setNome("TesteUpdate");
-		alunoDao.save(a1);
+	// 	List<Aluno> result = alunoDao.search(a);
+	// 	assertEquals(1, result.size());
+	// 	assertEquals("036.464.453-27", result.get(0).getFiliado().getCpf());
 		
-		Aluno a2 = alunoDao.get(a1);
-		assertEquals("TesteUpdate", a2.getFiliado().getNome());
-		assertEquals(1, alunoDao.list().size());
-	}
-
-	@Test
-	public void deleteAluno() throws Exception{ // Teste ID 6
-
-		clearDatabase();
-		assertEquals(0, alunoDao.list().size());
-		
-		alunoDao.save(aluno);
-		assertEquals(1, alunoDao.list().size());
-		assertEquals("Aécio", aluno.getFiliado().getNome());
-
-		assertEquals(1, alunoDao.list().size());
-
-		Aluno a1 = alunoDao.get(aluno);
-
-		alunoDao.delete(a1);
-
-		assertEquals(0, alunoDao.list().size());
-	}
-	
-	@Test
-	public void testSearchAluno() throws Exception{
-		clearDatabase();
-		alunoDao.save(aluno);
-		
-		Filiado f = new Filiado();
-		f.setNome("Aécio");
-		Aluno a = new Aluno();
-		a.setFiliado(f);
-		
-		List<Aluno> result = alunoDao.search(a);
-		assertEquals(1, result.size());
-		assertEquals("036.464.453-27", result.get(0).getFiliado().getCpf());
-		
-		clearDatabase();
-		assertEquals(0, alunoDao.search(a).size());
-	}
+	// 	clearDatabase();
+	// 	assertEquals(0, alunoDao.search(a).size());
+	// }
 	
 	@AfterClass
 	public static void closeDatabase(){
